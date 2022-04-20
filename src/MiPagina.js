369271966f9127207cc133063efcd3d8879fb2be
Sebas744 <89553749+Sebas744@users.noreply.github.com>
2022-04-20 17:38:35 -0300

    Tarea Clase 3 Finocchietti React

---
+++ src/MiPagina.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header.js"
+import Footer from "./components/Footer.js"
+import Main from "./components/ItemListContainer.js"
+
+
+const MiPagina = () => {
+    return( <>        
+        <Header/> 
+        <Main>
+        <p>Proximamente información para inversores</p>           
+        </Main>
+        <Footer/>        
+        </>
+        )
+}
+
+export default MiPagina
+
